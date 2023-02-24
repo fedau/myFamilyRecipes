24dd9de57b2c859be90e798bb9dc4af3bd366972
fedau <fedau.d@hotmail.com>
2023-02-24 18:45:51 +0000

    model image making constructor and getters and setters

---
+++ recipeService/src/main/java/com/backend/recipes/models/Image.java
@@ -0,0 +1,50 @@
+package com.backend.recipes.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+    private String image;
+
+    @JsonIgnoreProperties({"images"})
+    @OneToOne
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    public Image(String image, Recipe recipe) {
+        this.image = image;
+        this.recipe = recipe;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
