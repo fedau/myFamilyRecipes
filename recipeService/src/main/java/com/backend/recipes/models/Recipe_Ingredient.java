d4e89ebc76bde823959855a1314b4aa232b653b3
fedau <fedau.d@hotmail.com>
2023-02-24 18:26:21 +0000

    model recipe_ingredient making constructor and getters and setters

---
+++ recipeService/src/main/java/com/backend/recipes/models/Recipe_Ingredient.java
@@ -0,0 +1,76 @@
+package com.backend.recipes.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "recipe_ingredients")
+public class Recipe_Ingredient {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_ingredient_id")
+    private Long id;
+
+    private double Quantity;
+    private String unit;
+
+    @JsonIgnoreProperties({"recipe_ingredients"})
+    @ManyToOne
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    @JsonIgnoreProperties({"recipe_ingredients"})
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+    public Recipe_Ingredient(double quantity, String unit, Recipe recipe, Ingredient ingredient) {
+        Quantity = quantity;
+        this.unit = unit;
+        this.recipe = recipe;
+        this.ingredient = ingredient;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        Quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+}
