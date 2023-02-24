07e508568ba3d6683c85632dab32583558e065ed
fedau <fedau.d@hotmail.com>
2023-02-24 17:52:35 +0000

    initial commit

---
+++ recipeService/src/main/java/com/backend/recipes/models/Recipe.java
@@ -0,0 +1,81 @@
+package com.backend.recipes.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private int cookingTime;
+
+    private String servings;
+
+    private String image;
+
+
+    public Recipe(String name, String description, int cookingTime, String servings, String image) {
+        this.name = name;
+        this.description = description;
+        this.cookingTime = cookingTime;
+        this.servings = servings;
+        this.image = image;
+    }
+
+    public Recipe(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCookingTime() {
+        return cookingTime;
+    }
+
+    public void setCookingTime(int cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public String getServings() {
+        return servings;
+    }
+
+    public void setServings(String servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
