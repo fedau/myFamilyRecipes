800e21315be1774e332c7476bd670e2db439e800
fedau <fedau.d@hotmail.com>
2023-02-24 19:17:51 +0000

    controller for Instructions with get mapping and post mapping

---
+++ recipeService/src/main/java/com/backend/recipes/controller/InstructionsController.java
@@ -0,0 +1,34 @@
+package com.backend.recipes.controller;
+
+import com.backend.recipes.models.Category;
+import com.backend.recipes.models.Instructions;
+import com.backend.recipes.repository.CategoryRepository;
+import com.backend.recipes.repository.InstructionsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class InstructionsController {
+
+    @Autowired
+    InstructionsRepository instructionsRepo;
+
+    @GetMapping(value = "/instructions")
+    public ResponseEntity<List<Instructions>> getAllInstructions(){
+        return new ResponseEntity<>(instructionsRepo.findAll(), HttpStatus.OK);
+    }
+    @GetMapping(value = "/instructions/{id}")
+    public ResponseEntity getInstructions(@PathVariable Long id){
+        return new ResponseEntity<>(instructionsRepo.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/instructions")
+    public ResponseEntity<Instructions> postCategory(@RequestBody Instructions instructions){
+        instructionsRepo.save(instructions);
+        return new ResponseEntity<>(instructions, HttpStatus.CREATED);
+    }
+}
