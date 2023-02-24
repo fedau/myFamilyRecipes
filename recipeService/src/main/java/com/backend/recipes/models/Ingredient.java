b86bbcdd342e93bcb8521e85b84300d9c6de5d06
fedau <fedau.d@hotmail.com>
2023-02-24 18:12:16 +0000

    creation of ingredient models with constructor getters and setters

--- recipeService/src/main/java/com/backend/recipes/models/Ingredient.java
+++ recipeService/src/main/java/com/backend/recipes/models/Ingredient.java
@@ -1,4 +1,34 @@
 package com.backend.recipes.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ingredients")
 public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
