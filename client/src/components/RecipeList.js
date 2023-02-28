61d21ff34e9f95626a0228ebf4cd543aa57e4995
fedau <fedau.d@hotmail.com>
2023-02-28 23:20:19 +0000

    recipe list mapping over all recipes

---
+++ client/src/components/RecipeList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+const RecipeList = ({recipes}) => {
+    if(recipes.length === 0){
+        return (<p>Loading...</p>)
+    }
+
+        const recipeElements = recipes.map((recipe, index) => {
+            return(
+                <li key={index}>
+                    <div>
+                        <Recipe recipe ={recipe} />
+            
+                    </div>
+                    </li>
+
+            )  
+        })
+  return (
+    <div>
+        <ul>
+            {recipeElements}
+        </ul>
+<p>Recipe list page</p>
+      
+    </div>
+  )
+}
+
+export default RecipeList
