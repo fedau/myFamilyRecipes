11a13612a9bcfb458a50ecb5c868f4d65f928a3a
fedau <fedau.d@hotmail.com>
2023-02-28 23:21:49 +0000

    request routes

---
+++ client/src/helpers/request.js
@@ -0,0 +1,37 @@
+class Request {
+    //Fetch data from api
+    async get(url){
+        const result = await fetch(url); //setup a proxy in package json because this is a react url right now
+        return result.json()
+    }
+
+    delete(url){
+        return fetch(url, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json"}
+        })
+    }
+
+    post(url, payload){
+        return fetch(url, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body:JSON.stringify(payload)
+        })
+    }
+
+
+        put(url, payload){
+            return fetch(url, {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body:JSON.stringify(payload)
+
+
+            })
+        }
+    }
+
+
+
+export default Request;
