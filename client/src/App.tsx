2a3247304e2a4da706d3de64cbf04c893ef04ec9
fedau <fedau.d@hotmail.com>
2023-02-28 23:18:49 +0000

    app loading recipecontainer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,18 +7,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Maincontainer from './containers/Maincontainer';
 import Homep from './containers/Homep';
 import RecipeContainer from './containers/RecipeContainer';
+import RecipeList from './components/RecipeList';
+import Favourites from './containers/Favourites';
+import { useAuth0 } from "@auth0/auth0-react";
+import AnotherBar from './components/NavBar';
+
 
 function App() {
+
+  // const {
+  //   user,
+  //   isAuthenticated,
+  //   loginWithRedirect,
+  //   logout,
+  // } = useAuth0();
+
   return (
-   <Router>
-    <div className="App">
-  {/* <Maincontainer/>*/}
-  <Routes>
-   <Route path='/' element={ <RecipeContainer/>}/> 
-  </Routes>
- <p>appp</p>
-    </div>
-   </Router>
+<Router>
+  <AnotherBar/>
+  <RecipeContainer/>
+</Router>
   );
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
+  <Auth0Provider
+    domain="fedautesting.eu.auth0.com"
+    clientId="87DjAloH00KRKD3UdjCdzD4ygMfWOshs"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>
   </React.StrictMode>
 );
 
